d6d9b8508d40d9332c7812901a6c98a64de0eced
Will Paxton <43458264+willpaxton@users.noreply.github.com>
2023-11-07 21:42:08 -0500

    Update Dock.cs
    Not all working yet

--- warehouse-project/Dock.cs
+++ warehouse-project/Dock.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace warehouse_project
 {
-    internal class Dock
+    public class Dock
     {
+        public String Id { get; set; } // probably should be incremental
+
+        Queue<Truck> Line = new Queue<Truck>();
+
+        public double TotalSales { get; set; } = 0;
+
+        public int TotalCrates { get; set; } = 0;
+
+        public int TotalTrucks { get; set; } = 0;
+
+        public int TimeInUse { get; set; } = 0;
+
+        public int TimeNotInUse { get; set; } = 0;
+
+        public void JoinLine(Truck truck)
+        {
+
+        }
+
+        public Truck SendOff()
+        {
+            
+        }
     }
 }
